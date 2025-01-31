ef6bb11092bb1eeebbf7a54f0572803840d0577e
Miguel-mmf <miguel.ferreira@cear.ufpb.br>
2025-01-31 12:24:51 -0300

    adiciona documentação e melhorias nas funções de manipulação de JSON e XLSX

--- create_xlsx_docs_from_json_file.py
+++ create_xlsx_docs_from_json_file.py
@@ -9,8 +9,23 @@ from tqdm import tqdm
 FILE_PATH: str = 'data/input_model.json'
 
 
-# Função para explorar o JSON e extrair os dados em profundidade
 def explore_json(data: dict, parent_key: str = '') -> list:
+    """Explores a JSON structure and extracts key-value pairs.
+
+    This function recursively navigates a dictionary representing a JSON
+    structure, flattening it into a list of tuples. Each tuple contains
+    the full key path, the type of the value, and the value itself.
+    It handles nested dictionaries and lists, extracting information
+    from each level.
+
+    Args:
+        data (dict): The JSON data as a dictionary.
+        parent_key (str, optional): The key path of the parent object. Defaults to "".
+
+    Returns:
+        list: A list of tuples, where each tuple contains the key path,
+            value type, and example value.
+    """
     items = []
     for key, value in data.items():
         new_key = f'{parent_key}.{key}' if parent_key else key
@@ -29,30 +44,57 @@ def explore_json(data: dict, parent_key: str = '') -> list:
 
 
 def read_json_file(file_path: str) -> dict:
+    """Reads a JSON file and returns its content as a dictionary.
+
+    This function opens the specified JSON file, reads its contents,
+    and parses it into a Python dictionary using the `json` module.
+    It handles UTF-8 encoding to support a wide range of characters.
+
+    Args:
+        file_path (str): The path to the JSON file.
+
+    Returns:
+        dict: The JSON data as a Python dictionary.
+    """
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
 
 def create_xlsx_docs(file_path: str = FILE_PATH) -> None:
+    """Creates XLSX documentation from a JSON file.
+
+    This function reads a JSON file, extracts its structure,
+    and generates an XLSX file containing documentation tables.
+    Each table represents a main key in the JSON data, with columns
+    for key paths, value types, examples, and additional information.
+
+    Args:
+        file_path (str, optional): The path to the JSON file.
+            Defaults to FILE_PATH.
+
+    Returns:
+        None
+    """
     data = read_json_file(file_path)
 
     if not data:
         raise ValueError('O arquivo JSON está vazio!')
 
-    if 'output' in file_path.split('/')[-1]:
-        content = data.get('result', {})
-    elif 'input' in file_path.split('/')[-1]:
-        content = data.get('content', {})
-    else:
-        raise ValueError(
-            "O arquivo JSON não possui a chave 'content' ou 'result'!"
-        )
-    content = content or data
+    # if 'output' in file_path.split('/')[-1]:
+    #     content = data.get('result', {})
+    # elif 'input' in file_path.split('/')[-1]:
+    #     content = data.get('content', {})
+    # else:
+    #     raise ValueError(
+    #         "O arquivo JSON não possui a chave 'content' ou 'result'!"
+    #     )
 
     tables = defaultdict(list)
-    for main_key, sub_dict in tqdm(content.items()):
+    main_keys_with_types = set()
+    for main_key, sub_dict in tqdm(data.items()):
         tqdm.write(f'Processando {main_key}...', end='\r')
+        main_keys_with_types.add((main_key, type(sub_dict).__name__))
         if isinstance(sub_dict, dict):
             records = explore_json(sub_dict)
             for record in records:
@@ -83,6 +125,15 @@ def create_xlsx_docs(file_path: str = FILE_PATH) -> None:
 
     output_filename = file_path.replace('.json', '.xlsx')
     with pd.ExcelWriter(output_filename) as writer:
+        
+        df = pd.DataFrame()
+        df['Chave primária'] = [main_key[0] for main_key in main_keys_with_types]
+        df['Tipo'] = [main_key[1] for main_key in main_keys_with_types]
+        df['Significado'] = None
+        df['Observações'] = None
+        df.fillna('---', inplace=True)
+        df.to_excel(writer, sheet_name='Chaves Principais', index=False)
+        
         for sheet_name, data in tables.items():
             df = pd.DataFrame(data)
             df['Significado'] = None
@@ -150,7 +201,21 @@ def create_xlsx_docs(file_path: str = FILE_PATH) -> None:
     print('Arquivo Excel gerado com sucesso!')
 
 
-def merge_equal_cells(sheet, col):
+def merge_equal_cells(sheet, col: str) -> None:
+    """Merges cells with equal values in a given column of an Excel sheet.
+
+    This function iterates through the specified column of the provided
+    `openpyxl` worksheet and merges contiguous cells that have the same
+    value. It starts from the first row and continues until the last row,
+    merging cells vertically whenever a sequence of identical values is found.
+
+    Args:
+        sheet: The `openpyxl` worksheet object.
+        col: The column letter (e.g., 'A', 'B', 'C') to process.
+
+    Returns:
+        None
+    """
     max_row = sheet.max_row
     start_row = 1
     while start_row <= max_row:
@@ -166,6 +231,19 @@ def merge_equal_cells(sheet, col):
 
 
 def config_xlsx(xlsx_path: str) -> None:
+    """Configures and styles an XLSX file.
+
+    This function takes the path to an XLSX file, loads it using `openpyxl`,
+    and applies various formatting styles. It sets fonts, fills, borders,
+    merges cells, and configures auto-filters to enhance the readability
+    and presentation of the data in the Excel sheets.
+
+    Args:
+        xlsx_path (str): The path to the XLSX file.
+
+    Returns:
+        None
+    """
     wb = load_workbook(xlsx_path)
 
     # Definir as cores de preenchimento
@@ -214,7 +292,7 @@ def config_xlsx(xlsx_path: str) -> None:
                 try:
                     if len(str(cell.value)) > max_length:
                         max_length = len(str(cell.value))
-                except:
+                except Exception:
                     pass
                 cell.alignment = Alignment(
                     horizontal='center', vertical='center'
@@ -224,7 +302,7 @@ def config_xlsx(xlsx_path: str) -> None:
                     col[0].value == 'Tipo'
                     and cell.value in mapping_color_per_type
                 ):
-                    cell.fill = mapping_color_per_type.get(cell.value, None)
+                    cell.fill = mapping_color_per_type.get(cell.value)
 
             adjusted_width = max_length + 8
             ws.column_dimensions[column].width = adjusted_width
